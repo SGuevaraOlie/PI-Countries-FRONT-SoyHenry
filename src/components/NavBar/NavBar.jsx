02a9edff85a643a42f01c31ee1e1216e4cb97c18
SGuevaraOlie <santiguevara98@gmail.com>
2024-01-28 23:14:39 -0300

    correccion

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,25 @@
 // Routing
-import { Link, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 // Components
 import SearchBar from '../SearchBar/SearchBar';
 // Helpers
 import PATHROUTES from '../../helpers/PathRoutes.helper';
 // Styles
 import styles from './NavBar.module.css'
-import { useDispatch } from 'react-redux';
-import { resetCountries } from '../../redux/actions';
 
 
 const NavBar = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+    const location = useLocation();
     const handleHomeClick = () => {
-        navigate('/home');
+        if (location.pathname === PATHROUTES.HOME){
+            window.location.reload();
+        }
     }
     return (
         <div className={styles.navMain}>
             <div className={styles.links}>
                 <Link to={PATHROUTES.LANDING} className={styles.title}><h1>Countries APP</h1></Link>
-                <a className={styles.title} href='/home'><h1>Home</h1></a>
+                <Link to={PATHROUTES.HOME} className={styles.title} onClick={handleHomeClick}><h1>Home</h1></Link>
                 <Link to={PATHROUTES.FORM} className={styles.title}><h1>Crear Actividades</h1></Link>
             </div>
             <Routes>
